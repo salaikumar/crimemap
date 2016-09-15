db2ba651b27cb2ada0835e27470de85cc33cac34
salaikumar <saravanan1115@gmail.com>
2016-09-15 17:43:52 +0530

    Fresh commit after two weeks. Back to Python + Flask since we got selected for HAck

--- dbhelper.py
+++ dbhelper.py
@@ -20,9 +20,10 @@ class DBHelper:
      try:
          # The following introduces a deliberate security flaw.
          #  See section on SQL injection below
-         query = "INSERT INTO crimes (description) VALUES ('{}');".format(data)
+         # SQL injection resolved using %s
+         query = "INSERT INTO crimes (description) VALUES (%s);"
          with connection.cursor() as cursor:
-             cursor.execute(query)
+             cursor.execute(query,data)
          connection.commit()
      finally:
          connection.close()
